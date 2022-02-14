492430cae4c9d2e2f6d9eefa3c2212a13e6f1b09
bosen L <bosenli2013@hotmail.com>
2022-02-13 23:20:57 -0500

    finished without commit

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const API_KEY_Secret = "52hout6rda3ps0yh6gtnb7um5y5tsdt4d9m9i4awd5k8dbi1uv";
 const API_KEY_ID = "dxxs7s4om8d66rbf0002jro15";
 const API_APP_TOKEN = "vLJPGCsLg4YQm3oXnNUk14osI";
 const API_APP_TOKEN_SECRET_KEY ="I89Z0_MTeHzxIOWPXdJ8HwJ3HVEeIOQrbC5E";
-//const button = document.querySelector("button");
-//const button = document.getElementsById('borough');
+
 const btns = document.querySelectorAll('button');
 //data needed: .borough , .descriptor , .agency:"NYPD", .resolution_description 
 //If the user doesn't input any number, make the default be 10
@@ -24,50 +23,44 @@ const response = async (limit = 10,borough='BROOKLYN') =>
         console.log(res.data);
         //output result in div
         let allData = res.data;
-        // let descriptor = res.data.descriptor;
-        // let resolution_description = res.data.resolution_description;
+
         
         for (let i = 0; i < allData.length; i++ ){
-          // let newDiv = document.createElement("div");
-          // let viewMoreButton = document.createElement("button");
-          // let list = document.createElement("li");
-          // //<div> <li>descriptor <button>    </li>
-          // viewMoreButton.textContent ="WHAT DID THE POILICE DO?";
-          // let infoDiv = document.createElement('div');
-          // viewMoreButton.addEventListener('click', async ()=>{
-          //   infoDiv.innerHTML = allData[i].resolution_description
-          // });
-      
-          output.innerHTML += `<li>${allData[i].descriptor}  <button id=${allData[i].unique_key}>WHAT DID THE POILICE DO? </button> </li>` //  ${allData[i].resolution_description}  https://stackoverflow.com/questions/43298962/setting-html-buttononclick-in-template-literal
+            
+            let newli = document.createElement('li');
+            newli.innerHTML = allData[i].descriptor;
 
-          //onclick= "myFunction('${i}')
-          //<button onclick= ${myFunction(i)}>WHAT DID THE POILICE DO? </button>
-         
-          let x = document.getElementById(allData[i].unique_key);
-     x.addEventListener("click",()=>{
-       console.log('hello');})
+            let newbutton = document.createElement("button");
+            newbutton.textContent="WHAT DID THE POLICE DO?";
 
-     }
-    //  var x = document.getElementById(allData[i].unique_key);
-    //  x.addEventListener("click",()=>{
-    //    console.log('hello');})
-    //  if (x.style.display === "none") {
-    //    x.style.display = allData[i].resolution_description;
-    //  } else {
-    //    x.style.display = "none";
-    //  }
-        
-    
-    }).catch(e => e);
-}
+            let descriptionDiv = document.createElement('div');
+            descriptionDiv.innerHTML = allData[i].resolution_description;
+            descriptionDiv.setAttribute("id",allData[i].unique_key)
+            
 
+            newli.appendChild(newbutton);
+            output.appendChild(newli);
+            output.appendChild(descriptionDiv);
+
+
+            descriptionDiv.innerHTML = "";
+            newbutton.addEventListener('click', function() {
+                if (descriptionDiv.innerHTML === allData[i].resolution_description){
+                    descriptionDiv.innerHTML = "";
+                }
+                else (descriptionDiv.innerHTML= allData[i].resolution_description);
+                })
+              };
+
+            }).catch(e => e);
+        }
 response();
 //https://www.codeinwp.com/snippets/add-event-listener-to-multiple-elements-with-javascript/
 
 //set input listener if not limit = 10
 var limitN;
 input.addEventListener('input', (e)=>{ 
-  if (e.target.value == 0 || e.target.value == null){
+  if (e.target.value == 0 || e.target.value == null || e.target.value == ""){
     limitN = 10;
   }
   else limitN = e.target.value
@@ -79,8 +72,3 @@ btns.forEach(function (i) {
       response(limitN , borough);
   });
 });
-// btn.addEventListener("click", () =>{
-//     let playerSelection = button.id;
-//     console.log(playerSelection);
-//     //response(city)
-// })
